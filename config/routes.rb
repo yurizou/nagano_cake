5d991f9773a3632bdc437d359d7538b01e446fff
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-06-22 15:09:06 +0000

    [Add-home controller]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
  
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
+  
  devise_for :customers, skip: [:passwords], controllers: {
    registrations: "public/registrations",
    sessions: 'public/sessions'
