815f6892369b275f8c46d67442cb075e9a9dcf89
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-06-24 02:28:38 +0000

    [Add]

---
+++ app/controllers/public/deliveries_controller.rb
@@ -0,0 +1,2 @@
+class Public::DeliveriesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,45 @@
 Rails.application.routes.draw do
- 
+
   root to: 'homes#top'
   get 'about' => 'homes#about'
+
+  scope module: :public do
+     get 'items' => 'public/items#index'
+     get 'items params[:id]' => 'items#show'
+ end
+
+  namespace :admin do
+    resources :items
+ end
+ 
+ scope module: :public do
+     resources :customers
+end
+
+ namespace :admin do
+     resources :customers
+end
+
+  scope module: :public do
+    resources :deliveries
+ end
+     
+
+ namespace :admin do
+    resources :deliveries
+end
   
+
  devise_for :customers, skip: [:passwords], controllers: {
    registrations: "public/registrations",
    sessions: 'public/sessions'
  }
- 
+
  devise_for :admin, skip: [:registrations, :passwords], controllers: {
    sessions: "admin/sessions"
  }
- 
- namespace :admin do
-    resources :items
- end
- 
- namespace :public do
-     resources :items
- end
- 
+
+
+
 end
 
