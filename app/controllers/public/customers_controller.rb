ea73b1670c0ad0d559522174408c57197d9682a7
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-07-02 03:39:47 +0000

    [Add-customers/show,edit]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,14 +1,16 @@
 class Public::CustomersController < ApplicationController
   def show
-    
+    @customer = Customer.find(params[:id])
   end
   
   def edit
-    
+    @customer = Customers.find(params[:id])
   end
   
   def update
-    
+    @customer = Customers.find(params[:id])
+    Customer.update(customer.params)
+    redirect_to customers_my_page_path
   end
   
   def confirm
@@ -17,5 +19,11 @@ class Public::CustomersController < ApplicationController
   
   def quit
     
+  end
+  
+   private
+
+  def customers_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :number, :email)
   end
 end
