a4df66210017f890d2e87c1785e3eaadced8f1bb
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-07-04 14:23:54 +0000

    [Add-admin-index]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,14 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
+    @item = Item.find(params[:id])
   end
   
   def show
     @items = Item.name
+  end
+  
+  def new
+    @item = Item.new
   end
 end
