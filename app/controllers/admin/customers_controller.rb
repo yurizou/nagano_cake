05a2bf6eaf2371d543939fc05fa6b0a7101cef02
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-07-10 10:59:25 +0000

    [Add-admin-customer-show]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,14 +5,21 @@ class Admin::CustomersController < ApplicationController
   end
   
   def show
-    
+    @customer = Customer.find(params[:id])
   end
   
   def update
-    
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_path(customer.id)
   end
   
   def edit
-    
+    @customer = Customer.find(params[:id])
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:id, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :number, :email, :is_delete)
   end
 end
