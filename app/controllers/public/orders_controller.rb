0f8db92a1e8c5831be336741b2258f3dfc19463a
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-07-15 14:39:42 +0000

    [Add-public-order]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -17,10 +17,20 @@ class Public::OrdersController < ApplicationController
   end
   
   def confirm
-    
+    @order = Order.new(order_params)
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.find(params[:id])
   end
   
   def create
     
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
