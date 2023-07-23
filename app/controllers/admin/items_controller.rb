a65331febf1af845522ca765b9ad3ceaec9fb1fb
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-07-23 14:11:03 +0000

    [Add]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,8 +10,11 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-     @item.save
-    redirect_to admin_item_path(@item.id)
+     if @item.save
+       redirect_to admin_item_path(@item.id)
+     else
+       render:new
+     end
 
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,12 @@ class Item < ApplicationRecord
  has_many :cart_items, dependent: :destroy
  has_many :order_details, dependent: :destroy
  
-
- 
+ validates :image, presence: true
+ validates :name,presence: true
+ validates :introduction, presence: true
+ validates :price, presence: true
+ validates :genre_id, presence: true 
+ validates :is_active, presence: true
  
  def with_tax_price
   (price * 1.1).round
