e992e25cfac750a99fbb9015ef27cbf1ea623eff
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-07-30 04:20:32 +0000

    [Add]

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
     @orders = Order.page(params[:page]).per(10)
-    
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_detail =@order.order_details
+   
   end
 
   def update
