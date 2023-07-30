0950820db3fbdd9c90f9de1af3da0d827f35c7db
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-07-30 14:44:33 +0000

    [Add]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  
-  
+  before_action :customer_state, only: [:create]
    #before_action :customer_state, only: [:create]
-   
-
+  
   # GET /resource/sign_in
   # def new
   #   super
@@ -16,22 +14,23 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
+
+
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-#   protected
+   protected
 
-# def customer_state
-#   @customer = Customer.find_by(email: params[:customer][:email])
-#   return if !@customer
-#   if @customer.valid_password?(params[:customer][:password])
-#   end
+ def customer_state
+   @customer = Customer.find_by(email: params[:customer][:email])
+   return if !@customer
+   if @customer.valid_password?(params[:customer][:password])
+   end
+ end
 
 #   # If you have extra params to permit, append them to the sanitizer.
 #   # def configure_sign_in_params
 #   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-#   # end
-#   end
- end
+end
