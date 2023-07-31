ae93f7f53463cabc60dd77a8e334254f0bf8d089
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-07-31 13:25:29 +0000

    [Add-Finished]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,7 +3,7 @@
 class Public::SessionsController < Devise::SessionsController
   before_action :customer_state, only: [:create]
    #before_action :customer_state, only: [:create]
-  
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -27,6 +27,9 @@ class Public::SessionsController < Devise::SessionsController
    @customer = Customer.find_by(email: params[:customer][:email])
    return if !@customer
    if @customer.valid_password?(params[:customer][:password])
+     if @customer.is_delete == true
+      redirect_to new_customer_registration_path
+     end
    end
  end
 
